8137aca7cf705f31d92c1f643dd44791b3c265d1
kristof1345 <kristofnemeth1345@gmail.com>
2024-12-13 21:31:23 +0100

    tildes

--- editor.rs
+++ editor.rs
@@ -1,9 +1,11 @@
-use crossterm::event::{read, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use std::io;
+use crossterm::cursor::MoveTo;
+use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
+use crossterm::execute;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType};
+use std::io::{self, stdout};
 
 pub struct Editor {
-    should_quit: bool,
+    should_quit: bool, // we don't have to declare that we want a mutatable field
 }
 
 impl Editor {
@@ -12,37 +14,73 @@ impl Editor {
     }
 
     pub fn run(&mut self) {
-        if let Err(err) = self.repl() {
-            panic!("{err:#?}");
-        }
-        println!("Goodbye\r\n");
+        Self::initialize().unwrap();
+        Self::draw_rows().unwrap();
+        let result = self.repl();
+        Self::terminate().unwrap();
+        result.unwrap();
+    }
+
+    fn initialize() -> Result<(), io::Error> {
+        enable_raw_mode()?;
+        Self::clear_screen()
+    }
+
+    fn terminate() -> Result<(), io::Error> {
+        disable_raw_mode()
+    }
+
+    fn clear_screen() -> Result<(), io::Error> {
+        let mut stdout = stdout();
+        execute!(stdout, Clear(ClearType::All))
     }
 
     fn repl(&mut self) -> Result<(), io::Error> {
-        enable_raw_mode()?;
         loop {
-            if let Key(KeyEvent {
-                code,
-                modifiers,
-                kind,
-                state,
-            }) = read()?
-            {
-                println!(
-                    "Code: {code:?} Modifiers: {modifiers:?} Kind: {kind:?} State: {state:?} \r"
-                );
-                match code {
-                    Char('q') if modifiers == KeyModifiers::CONTROL => {
-                        self.should_quit = true;
-                    }
-                    _ => (),
-                }
-            }
+            let event = read()?;
+            self.evaluate_events(&event);
+            self.refresh_screen()?;
+
             if self.should_quit {
                 break;
             }
         }
-        disable_raw_mode()?;
+        Ok(())
+    }
+
+    fn evaluate_events(&mut self, event: &Event) {
+        if let Key(KeyEvent {
+            code, modifiers, ..
+        }) = event
+        {
+            match code {
+                Char('q') if *modifiers == KeyModifiers::CONTROL => {
+                    self.should_quit = true;
+                }
+                _ => (),
+            }
+        }
+    }
+
+    fn refresh_screen(&self) -> Result<(), io::Error> {
+        if self.should_quit {
+            Self::clear_screen()?;
+            print!("Goodbye.\r\n");
+        }
+        Ok(())
+    }
+
+    fn draw_rows() -> Result<(), io::Error> {
+        let (_cols, rows) = size().unwrap();
+        let mut stdout = stdout();
+
+        for row in 0..rows {
+            execute!(stdout, MoveTo(0, row))?;
+            print!("~");
+        }
+
+        execute!(stdout, MoveTo(0, 0))?;
+
         Ok(())
     }
 }
--- main.rs
+++ main.rs
@@ -3,5 +3,6 @@ mod editor;
 use editor::Editor;
 
 fn main() {
-    Editor::default().run();
+    Editor::default().run(); // this isn't flipping off the borrow checker because we are not creating a variable here
+                             // if we were to store the output of this in a variable, the borrow checker would be mad at us
 }
