3e40f093176082766c26d13c224ff2fe7f04233d
kristof1345 <kristofnemeth1345@gmail.com>
2024-12-19 21:59:47 +0100

    opens files - bug: renders way too many empty lines

--- editor/view/buffer.rs
+++ editor/view/buffer.rs
@@ -1,11 +1,15 @@
+use std::vec::Vec;
+
 pub struct Buffer {
     pub lines: Vec<String>,
+    pub is_empty: bool,
 }
 
 impl Default for Buffer {
     fn default() -> Self {
         Self {
-            lines: vec!["Hello, world!".to_string()],
+            lines: Vec::new(),
+            is_empty: true,
         }
     }
 }
